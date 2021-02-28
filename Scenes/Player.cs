a8e30f2ac2a21996d1039c8cda482c6650178da7
jerrychoux <jerrychoux@163.com>
2021-02-28 13:36:31 +0800

    添加玩家

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -3,19 +3,67 @@ using System;
 
 public class Player : Area2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    [Export]
+    public int speed = 400;
+    private Vector2 _screenSize;
     public override void _Ready()
     {
-        GetNode<AnimatedSprite>("AnimatedSprite").Play();
+        _screenSize = GetViewport().Size;
+        // Hide();
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //      
-    //  }
+    public override void _Process(float delta)
+    {
+        var velocity = new Vector2();
+        if (Input.IsActionPressed("ui_right"))
+        {
+            velocity.x += 1;
+        }
+        if (Input.IsActionPressed("ui_left"))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.IsActionPressed("ui_down"))
+        {
+            velocity.y += 1;
+        }
+        if (Input.IsActionPressed("ui_up"))
+        {
+            velocity.y -= 1;
+        }
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * speed;
+            animatedSprite.Play();
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+        if (velocity.x < 0)
+        {
+            animatedSprite.FlipH = true;
+        }
+        else
+        {
+            animatedSprite.FlipH = false;
+        }
+        if (velocity.x != 0)
+        {
+            animatedSprite.Animation = "walk";
+            animatedSprite.FlipV = false;
+        }
+        else if (velocity.y != 0)
+        {
+            animatedSprite.Animation = "up";
+            animatedSprite.FlipV = velocity.y > 0;
+        }
+
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+            y: Mathf.Clamp(Position.y, 0, _screenSize.y)
+        );
+    }
 }
